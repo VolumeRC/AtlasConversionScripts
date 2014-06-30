4f0cf6cbdbf5ef1d3ba1ac012156379e671216e8
Ander Arbelaiz <arbelaitz@gmail.com>
2014-07-01 00:40:57 +0200

    Added support to compute the gradient

--- src/convertNRRD.py
+++ src/convertNRRD.py
@@ -13,30 +13,35 @@ import os, errno
 import sys
 import math
 import array
-from PIL import Image #this is required to manage the images
+#this is required to manage the images
+try:
+	from PIL import Image 
+except ImportError:
+	import Image
+try:
+	import numpy as np
+	from scipy import ndimage, misc
+except ImportError:
+	print 'You need scipy.ndimage, scipy.misc and Numpy (http://numpy.scipy.org/)' 
+	exit()
 
-# Bytescale function from SciPy misc
-def bytescale(data, high=255, low=0):
-	if data.dtype == np.uint8:
-		return data
+# Standard deviation for Gaussian kernel 
+sigmaValue = 1
 
-	if high < low:
-		raise ValueError("`high` should be larger than `low`.")
+def normalize(inputData):
+	old_min = inputData.min()
+	old_range = inputData.max()-old_min
+	return (inputData-old_min)/old_range
 
-	cmin = data.min()
-	cmax = data.max()
-
-	cscale = cmax - cmin
-	if cscale < 0:
-		raise ValueError("`cmax` should be larger than `cmin`.")
-	elif cscale == 0:
-		cscale = 1
-
-	scale = float(high - low) / cscale
-	bytedata = (data * 1.0 - cmin) * scale + 0.4999
-	bytedata[bytedata > high] = high
-	bytedata[bytedata < 0] = 0
-	return np.cast[np.uint8](bytedata) + np.cast[np.uint8](low)
+#This function calculates the gradient from a 3 dimensional numpy array
+def calculateGradient(arr):
+	r = np.zeros(arr.shape)
+	g = np.zeros(arr.shape)
+	b = np.zeros(arr.shape)
+	ndimage.gaussian_filter1d(arr, sigma=sigmaValue, axis=1, order=1, output=r)
+	ndimage.gaussian_filter1d(arr, sigma=sigmaValue, axis=0, order=1, output=g)
+	ndimage.gaussian_filter1d(arr, sigma=sigmaValue, axis=2, order=1, output=b)
+	return normalize(np.concatenate((r[...,np.newaxis],g[...,np.newaxis],b[...,np.newaxis]),axis=3))
 
 #This function simply loads a NRRD file and returns a compatible Image object
 def loadNRRD(filename):
@@ -58,21 +63,25 @@ def ImageSlices2TiledImage(filename, loadImgFunction=loadNRRD):
 	slicesPerAxis = int(math.ceil(math.sqrt(numberOfSlices)))
 	atlasArray = np.zeros((volumeSize[0]*slicesPerAxis, volumeSize[1]*slicesPerAxis))
 
+	gradientData = calculateGradient(data)
+	atlasGradientArray = np.zeros((volumeSize[0]*slicesPerAxis, volumeSize[1]*slicesPerAxis, 3))
+
 	for i in range(0, numberOfSlices):
 		row = int((math.floor(i/slicesPerAxis)) * volumeSize[0])
 		col = int((i%slicesPerAxis) * volumeSize[1])
 		box = (row, col, int(row+volumeSize[0]), int(col+volumeSize[1]))
 		atlasArray[box[0]:box[2],box[1]:box[3]] = data[:,:,i]
+		atlasGradientArray[box[0]:box[2],box[1]:box[3],:] = gradientData[:,:,i,:]
 
 	#From numpy to PIL image
-	bytedata = bytescale(atlasArray, high=255, low=0)
-	imout = Image.frombytes('L', atlasArray.shape, bytedata.tostring())
+	imout = misc.toimage(atlasArray, mode="L")
+	gradient = misc.toimage(atlasGradientArray, mode="RGB")
 
-	return imout, volumeSize, numberOfSlices, slicesPerAxis
+	return imout, gradient, volumeSize, numberOfSlices, slicesPerAxis
 
 #This functions takes a (tiled) image and writes it to a png file with base filename outputFilename.
 #	It also writes several versions in different sizes determined by dimensions
-def WriteVersions(tileImage,outputFilename,dimensions=[8192,4096,2048,1024]):
+def WriteVersions(tileImage,tileGradient,outputFilename,dimensions=[8192,4096,2048,1024]):
 	try:
 		print 'Creating folder',os.path.dirname(outputFilename),'...',
 		os.makedirs(os.path.dirname(outputFilename))
@@ -87,16 +96,24 @@ def WriteVersions(tileImage,outputFilename,dimensions=[8192,4096,2048,1024]):
 	print "Writing complete image: "+outputFilename+"_full.png"
 	try:
 		tileImage.save(outputFilename+"_full.png", "PNG")
+		if tileGradient:
+			tileGradient.save(outputFilename+"_gradient_full.png", "PNG")
 	except:
 		print "Failed writing ",outputFilename+"_full.png"
 	for dim in dimensions:
 		if tileImage.size[0] > dim :
 			print "Writing "+str(dim)+"x"+str(dim)+" version: "+outputFilename+"_"+str(dim)+".png"
-			tmpImage = tileImage.resize((dim,dim))
 			try:
+				tmpImage = tileImage.resize((dim,dim))
 				tmpImage.save(outputFilename+"_"+str(dim)+".png", "PNG")
 			except:
 				print "Failed writing ",outputFilename,"_",str(dim),".png"
+			if tileGradient:
+				try:
+					tmpImage = tileGradient.resize((dim,dim))
+					tmpImage.save(outputFilename+"_gradient_"+str(dim)+".png", "PNG")
+				except:
+					print "Failed writing ",outputFilename,"_gradient_",str(dim),".png"
 
 #This function lists the files within a given directory dir
 def listdir_fullpath(d):
@@ -122,14 +139,12 @@ def main(argv=None):
 	if len(filenameNRRD) > 0:
 		try:
 			global nrrd
-			global np
-			import numpy as np
 			import nrrd
 		except:
-			print "You need pynrrd package (sudo easy_install pynrrd) and numpy (http://numpy.scipy.org/) to do this!"
+			print "You need pynrrd package (sudo easy_install pynrrd) to do this!"
 			return 2
 		#From nrrd files
-		imgTile, sliceResolution, numberOfSlices, slicesPerAxis = ImageSlices2TiledImage(filenameNRRD,loadNRRD)
+		imgTile, gradientTile, sliceResolution, numberOfSlices, slicesPerAxis = ImageSlices2TiledImage(filenameNRRD,loadNRRD)
 	else:
 		print "No NRRD file found in that folder, check your parameters or contact the authors :)."
 		return 2
@@ -154,7 +169,7 @@ def main(argv=None):
 		print "Created",argv[2]+"_AtlasDim.txt","containing dimensions (total slices, slices per axis):",(numberOfSlices,(slicesPerAxis,slicesPerAxis))
 
 	#Output is written in different sizes
-	WriteVersions(imgTile, argv[2])
+	WriteVersions(imgTile, gradientTile, argv[2])
 
 if __name__ == "__main__":
 	sys.exit(main())
